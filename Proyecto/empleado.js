7dfa5f244facfe640b9f9eefbd27caa460ffd202
jhon03 <hoyosb90@gmail.com>
2022-11-21 20:50:24 -0500

    proyecto actualizado

---
+++ Proyecto/empleado.js
@@ -0,0 +1,20 @@
+class Empleado {
+    constructor (nombres, apellidos, direccion, cargo, eps, seguridadSocial, fechaNacimiento)   {           
+    
+    this.nombres= String;
+    this.apellidos= String;
+    this.direccion= String;
+    this.cargo= String; 
+    this.eps= String; 
+    this.seguridadSocial= String; 
+    this.fechaNacimiento= Date; 
+
+    
+}
+
+consultarTurno () {
+
+
+    console.log("su turno es: 2:00 a 10:.00 p.m")
+}
+}

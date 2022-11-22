b986855168adaf6015355a2cf664d26853094b66
Eve2764 <evelynforero2764@gmail.com>
2022-11-22 18:56:56 -0500

    get and set

--- Proyecto/empleado.js
+++ Proyecto/empleado.js
@@ -17,4 +17,72 @@ consultarTurno () {
 
     console.log("su turno es: 2:00 a 10:.00 p.m")
 }
+
+    get nombres (){
+      return this._nombres;
+  }
+
+/**
+* @param {any} nombres
+ */
+set nombre(nombres) {
+ this._nombres = nombres;
+  }
+get apellidos() {
+      return this._apellidos;
+  }
+
+/**
+       * @param {any} apellidos
+       */
+set apellido(apellidos){
+      this._apellidos = apellidos;
+  }
+get direccion () {
+      return this._direccion;
+  }
+/**
+       * @param {any} direccion
+       */
+set direccion(direccion) {
+      this._direccion = direccion;
+  }
+get cargo() {
+      return this._cargo;
+  }
+/**
+       * @param {any} cargo
+       */
+set cargo(cargo) {
+      this._cargo = cargo;
+  }
+get eps() {
+      return this._eps;
+  }
+/**
+       * @param {any} eps
+       */
+set eps(eps) {
+      this._eps = eps;
+  }
+get seguridadSocial() {
+      return this._seguridadSocial;
+  }
+/**
+        * @param {any} seguridadSocial
+        */
+set seguridadSocial(seguridadSocial) {
+      this._seguridadSocial = seguridadSocial;
+  }
+get fechaNacimiento() {
+      return this._fechaNacimiento
+  }
+/**
+        * @param {any} fechaNacimiento
+        */
+
+set fechaNacimiento(fechaNacimiento) {
+      this._fechaNacimiento = fechaNacimiento;
+  }
+
 }
